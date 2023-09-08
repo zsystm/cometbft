e67b9a36ebd43b45a25d541d7f5fbbfd047aad42
hvanz <hernan.vanzetto@gmail.com>
2023-09-08 14:18:12 +0200

    e2e: Fix log_level

--- test/e2e/pkg/testnet.go
+++ test/e2e/pkg/testnet.go
@@ -14,6 +14,7 @@ import (
 	"text/template"
 	"time"
 
+	"github.com/cometbft/cometbft/config"
 	"github.com/cometbft/cometbft/crypto"
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	"github.com/cometbft/cometbft/crypto/secp256k1"
@@ -203,7 +204,7 @@ func NewTestnetFromManifest(manifest Manifest, file string, ifd InfrastructureDa
 		testnet.LoadTxSizeBytes = defaultTxSizeBytes
 	}
 	if testnet.LogLevel == "" {
-		testnet.LogLevel = "true"
+		testnet.LogLevel = config.DefaultLogLevel
 	}
 
 	for _, name := range sortNodeNames(manifest) {
