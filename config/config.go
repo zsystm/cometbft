03d94cbd9f3b22e6d22bae1c37eda4df4e4efe7d
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2024-01-09 16:59:58 +0100

    Reduced timeoutCommit

--- config/config.go
+++ config/config.go
@@ -1145,7 +1145,7 @@ func DefaultConsensusConfig() *ConsensusConfig {
 		TimeoutPrevoteDelta:              500 * time.Millisecond,
 		TimeoutPrecommit:                 1000 * time.Millisecond,
 		TimeoutPrecommitDelta:            500 * time.Millisecond,
-		TimeoutCommit:                    1000 * time.Millisecond,
+		TimeoutCommit:                    300 * time.Millisecond,
 		SkipTimeoutCommit:                false,
 		CreateEmptyBlocks:                true,
 		CreateEmptyBlocksInterval:        0 * time.Second,
