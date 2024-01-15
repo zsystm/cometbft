d1fca9bea513830aeb17d0d83ad0d666c01e2a7b
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2024-01-15 09:40:35 +0100

    Increased timeout setup and localnet setup changes

--- test/loadtime/basic.sh
+++ test/loadtime/basic.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 # A basic invocation of the loadtime tool.
 
 ./build/load \
-    -c 1 -T 10 -r 1000 -s 1024 \
+    -c 1 -T 10 -r 10 -s 1048576 \
     --broadcast-tx-method sync \
-    --endpoints ws://localhost:26657/v1/websocket
-
+    --endpoints ws://localhost:26657/v1/websocket,ws://localhost:26660/v1/websocket 
