cd569588a69d54561912ff6c9c011b0a1e2c4db3
hvanz <hernan.vanzetto@gmail.com>
2024-01-22 11:01:20 +0100

    mempool: Allow very big txs only in first 1/4 of the mempool

--- mempool/clist_mempool.go
+++ mempool/clist_mempool.go
@@ -369,7 +369,14 @@ func (mem *CListMempool) isFull(txSize int) error {
 		txsBytes = mem.SizeBytes()
 	)
 
-	if memSize >= mem.config.Size || int64(txSize)+txsBytes > mem.config.MaxTxsBytes {
+	bytesCapacity := mem.config.MaxTxsBytes
+
+	// Very big txs are only allowed in the first quarter of the mempool.
+	if txSize > mem.config.MaxTypicalTxBytes {
+		bytesCapacity /= 4
+	}
+
+	if memSize >= mem.config.Size || int64(txSize)+txsBytes > bytesCapacity {
 		return ErrMempoolIsFull{
 			NumTxs:      memSize,
 			MaxTxs:      mem.config.Size,
